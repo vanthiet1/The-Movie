0ac7494e0e6666ffbce412cb8a90985ede13e40c
Văn Thiết <tretrauthiet@gmail.com>
2023-12-11 17:41:18 +0700

    movie

--- src/layouts/components/article.js
+++ src/layouts/components/article.js
@@ -28,22 +28,22 @@ const Article = () => {
                 <div className='p-10 w-full'>
                     <h1 className='text-5xl'>Phim đề cử</h1>
                 </div>
-                <div className='flex items-center p-10 gap-8 w-auto overflow-auto relative'>
+                <div className='flex items-center p-10 gap-8 w-auto overflow-x-auto relative'>
                     <div 
                     onClick={handleLeftArrow} 
-                    className="absolute z-10 left-5 bg-slate-500 p-5 rounded-full opacity-70">
+                    className="absolute z-10 left-5 bg-slate-500 hover:bg-slate-700 p-5 rounded-full opacity-70">
                     <IconleftArow />
                     </div>
                     <div 
                     onClick={handleRightArrow} 
-                    className="absolute z-10 right-5 bg-slate-500 p-5 rounded-full opacity-70"
+                    className="absolute z-10 right-5 bg-slate-500 hover:bg-slate-700 p-5 rounded-full opacity-70"
                     >
                     <IconrightArow />
                     </div>
 
                     {data.length > 0 && data.map((itemMovie,index) => (
                         <div key={itemMovie.id} className='relative'>
-                            <div className='relative w-96 h-auto transform hover:scale-110 transition-transform ease-in duration-300'>
+                            <div className='relative w-96 h-auto transform hover:scale-110 transition-transform ease-in-out duration-500' style={{transform: `translateX(${-currentIndex * 200}%)`}}>
                                 <img className='brightness-110 rounded-2xl object-cover w-full' src={`https://image.tmdb.org/t/p/w500${itemMovie.poster_path}`} alt="" />
                                 <div className='absolute bottom-0 bg-black w-full p-5 opacity-80 text-center'>
                                     <span className='w-max'>{itemMovie.original_title}</span>
